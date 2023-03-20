3546e9fa8122e7ae6818a9987efe57e5fb6e8c5c
AtomMes <atomik.2005@mail.ru>
2023-03-20 20:59:37 +0400

    Add: Webpack fix & Alert icons fix

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,15 +1,16 @@
-import React, { FC } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import useStatusDetector from "../../hooks/useStatusDetector";
-import styles from "./Alert.module.css";
+import React, { FC } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import useStatusDetector from '../../hooks/useStatusDetector';
+import styles from './Alert.module.css';
+import '../../styles.css';
 
 export interface AlertProps {
   heading: string;
   message: string;
-  status: "success" | "error" | "info" | "warning";
+  status: 'success' | 'error' | 'info' | 'warning';
 }
 
-const Alert: FC<AlertProps> = ({ heading, message, status }, ...props) => {
+const Alert: FC<AlertProps> = ({ heading, message, status }) => {
   const { color, icon } = useStatusDetector(status);
 
   return (
@@ -17,7 +18,9 @@ const Alert: FC<AlertProps> = ({ heading, message, status }, ...props) => {
       <div className={styles.wrapper}>
         <div className={styles.data}>
           <div className={styles.border} style={{ backgroundColor: color }} />
-          <div className={styles.icon} style={{ backgroundColor: color }}>
+          <div
+            className={styles.icon}
+            style={{ backgroundColor: color, paddingLeft: status === 'warning' ? '2.5px' : '' }}>
             <FontAwesomeIcon icon={icon} />
           </div>
           <div className={styles.heading}>{heading}</div>
